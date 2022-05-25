ac2468a5eaf3e00e2d7aab778546bcafc9ecd7dc
ujjwal6792 <ujjwalsharma.6792@gmail.com>
2022-05-25 23:51:36 +0530

    rupee sign

--- src/components/Payment.js
+++ src/components/Payment.js
@@ -144,7 +144,7 @@ function Payment() {
                                         value={getBasketTotal(basket)}
                                         displayType={"text"}
                                         thousandSeparator={true}
-                                        prefix={"$"}
+                                        prefix={"₹​"}
                                     />
                                     <button disabled={processing || disabled || succeeded}>
                                         <span>{processing ? <p>Processing</p> : "Buy Now"}</span>
